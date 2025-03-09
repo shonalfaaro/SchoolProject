53064c9ea20d1216b353742191c1b307b4ba0324
shonalfaaro <shonalfaaro@gmail.com>
2025-03-09 06:20:09 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main() {
+    string name = "John";
+    int age = 25;
+    double gpa = 3.7;
+    cout << "Hello, my name is " << name << " and I am " << age << " years old with a GPA of " << gpa << endl;
+    return 0;
+}
