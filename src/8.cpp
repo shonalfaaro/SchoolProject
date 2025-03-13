51dbe6a609b5edb0997b256ddcb7a2d1d27200da
shonalfaaro <shonalfaaro@gmail.com>
2025-03-13 11:37:39 +0000

    Add 8.cpp

---
+++ src/8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+  string name = "John Doe";
+  int age = 20;
+  double gpa = 3.5;
+  cout << "Name: " << name << endl;
+  cout << "Age: " << age << endl;
+  cout << "GPA: " << gpa << endl;
+  return 0;
+}
